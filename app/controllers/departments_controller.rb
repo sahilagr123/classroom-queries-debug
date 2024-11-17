6591216b836bf142b255b0b661719992bfcb97c9
sahilagr123 <sahilagr@uchicago.edu>
2024-11-17 23:29:25 +0000

    a

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -7,7 +7,8 @@ class DepartmentsController < ApplicationController
 
   def show
     the_id = params.fetch("path_id")
-    @department = Department.where({:id => the_id })
+    matching_record = Department.where({:id => the_id })
+    @department = matching_record.at(0)
 
     render({ :template => "departments/show" })
   end
